341b0f88f65135cdb2412acc0a347113a1b8827a
mhmdhalawi <mohammadhalawi93@gmail.com>
2021-11-01 17:19:02 +0200

    add report and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
+const root: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: [User, Report],
+  synchronize: true,
+};
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot(root), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
