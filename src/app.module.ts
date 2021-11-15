7805b54bb14358c7dcbbecdabcd70c382bd62701
mhmdhalawi <mohammadhalawi93@gmail.com>
2021-11-15 16:28:17 +0200

    add e2e testing for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
@@ -6,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+
+const cookieSession = require('cookie-session');
 
 const root: TypeOrmModuleOptions = {
   type: 'sqlite',
@@ -16,6 +19,24 @@ const root: TypeOrmModuleOptions = {
 @Module({
   imports: [TypeOrmModule.forRoot(root), UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['testkey'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Auth indication system (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup req', () => {
+    const email = 'moudi2@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '123456', username: 'moudi2' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toBe(email);
+      });
+  });
+});
